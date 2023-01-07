5c542c129a29e616c23e0295479fc658f737bec6
officialharikrishnan <harikrishnanpnr001@gmail.com>
2023-01-07 18:19:34 +0530

    mongo atlas auth added to env file

--- model/dbConnection/connection.js
+++ model/dbConnection/connection.js
@@ -6,9 +6,8 @@ const state = {
 
 // eslint-disable-next-line func-names
 module.exports.connect = function (done) {
-  const url = 'mongodb+srv://Harikrishnan:TeTV9IzmTa9UY1jZ@cluster0.eafyto8.mongodb.net/?retryWrites=true&w=majority';
+  const url = `mongodb+srv://Harikrishnan:${process.env.MONGO}@cluster0.eafyto8.mongodb.net/?retryWrites=true&w=majority`;
   const dbname = 'Wisdom';
-
   // eslint-disable-next-line consistent-return
   mongoClient.connect(url, (err, data) => {
     if (err) return done(err);
